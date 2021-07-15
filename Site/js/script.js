28cb7742d7752977e474f6014af161e6b373bf61
Makeavel <dpseutroco@gmail.com>
2021-07-14 21:06:17 -0300

    Subindo Site

---
+++ Site/js/script.js
@@ -0,0 +1,9 @@
+$('.ImgMenuLateral').click(function(){
+    $('.gridMenuLateral').toggleClass('mostra');
+});
+$('.header').click(function(){
+    $('.gridMenuLateral').toggleClass('recolhe')
+});
+$('.email').click(function(){
+    $('.globomail').toggleClass('mostra')
+});
